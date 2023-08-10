45db0ff6080a44e37a9df6bdef9111f0975c0927
hoda <nooralsmaa71@yahoo.com>
2023-08-10 11:54:46 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,12 @@
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Description: print a quote using write function
- * ssize_t write(int fd, const void *buf, size_t count);
-
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
  * Return: Always 1
  */
+
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, quo, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
