12706b23fe218494db0a5c2318aeee096d1b96be
hoda <nooralsmaa71@yahoo.com>
2023-08-11 07:16:32 -0700

    noQ

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print lowercase alphabet
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char let = 'a';
+
+	/*print a -z*/
+	while (let <= 'z', let != q, e)
+	{
+		putchar(let);
+		let++;
+	}
+	return (0)
+}
