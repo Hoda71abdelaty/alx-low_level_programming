4f36ba6a7bc9cedeb15d1424265afd726f7026fb
hoda <nooralsmaa71@yahoo.com>
2023-08-21 12:37:32 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates random valid passwards
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char passward[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
+
+	srand(time(0));
+	while (sum < 2772)
+	{
+		passward[index] = 33 + rand() % 94;
+		sum += passward[index++];
+	}
+
+	passward[index] = '\0';
+	if (sum != 2772)
+	{
+	diff_half1 = (sum - 2772) / 2;
+	diff_half2 = (sum - 2772) / 2;
+
+	if ((sum - 2772) % 2 != 0)
+	diff_half1++;
+
+	for (index = 0; passward[index]; index++)
+
+	{
+		if (passward[index] >= (33 diff_half1))
+		{
+			passward[index] -= diff_half1;
+			break;
+		}
+	}
+	for (index = 0; passward[index]; index++)
+
+	{
+		if (passward[index] >= (33 + diff_half2))
+		{
+		passward[index] -= diff_half2;
+			break;
+		}
+	}
+	}
+	printf("%s", passward);
+	return (0);
+}
+
